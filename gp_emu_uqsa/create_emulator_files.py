71eb326d99df46c98a0db0c67b448e9fb5841bbf
samcoveney <coveney.sam@gmail.com>
2016-11-24 12:33:56 +0000

    Fixed create_emulator_files() function to work with gp_emu_uqsa

--- gp_emu_uqsa/create_emulator_files.py
+++ gp_emu_uqsa/create_emulator_files.py
@@ -3,11 +3,12 @@ from builtins import input
 import os as __os
 
 def create_emulator_files():
+    print("This will create a new directory containing files for GP_emu_UQSA")
     name = input("Name of emulator: ")
     if not __os.path.exists(name):
         __os.makedirs(name)
     else:
-        print("That folder already exists, aborting")
+        print("That directory already exists, aborting")
         __os.sys.exit()
 
     inputs = input("Number of inputs: ")
@@ -23,10 +24,12 @@ def create_emulator_files():
     basis_str=""
     basis_inf=""
     beta=""
+    delta=""
     for i in range(0,inputs):
         basis_str = basis_str + " x"
         basis_inf = basis_inf + " " + str(i)
         beta = beta + " 1.0"
+        delta = delta + " 1.0"
 
 
     print("Creating beliefs file...") 
@@ -37,9 +40,11 @@ def create_emulator_files():
         bf.write("basis_inf NA" + basis_inf + "\n")
         bf.write("beta 1.0" + beta + "\n")
         bf.write("fix_mean F\n")
-        bf.write("kernel gaussian()\n")
-        bf.write("delta [ ]\n")
-        bf.write("sigma [ ]\n")
+        bf.write("delta" + delta + "\n")
+        bf.write("sigma 1.0\n")
+        bf.write("nugget 0.0\n")
+        bf.write("fix_nugget T\n")
+        bf.write("mucm F\n")
 
     inputs_filename=name + "_inputs"
     outputs_filename=name + "_outputs"
@@ -51,16 +56,15 @@ def create_emulator_files():
         cf.write("tv_config 10 0 1\n")
         cf.write("delta_bounds [ ]\n")
         cf.write("sigma_bounds [ ]\n")
+        cf.write("nugget_bounds [ ]\n")
         cf.write("tries 1\n")
-        cf.write("constraints T\n")
-        cf.write("stochastic F\n")
-        cf.write("constraints_type bounds\n")
+        cf.write("constraints bounds\n")
 
     print("Inputs and outputs files named",inputs_filename,"&",outputs_filename,"in the config file. Remember to include the input and output files in the new directory.")
 
     
-    sens = input("Include sensitivity routines? Y/[n]: ")
-    if sens == "Y":
+    sens = input("Include sensitivity routines? y/[n]: ")
+    if sens == "y":
         sensScript = True
     else:
         sensScript = False
@@ -68,15 +72,15 @@ def create_emulator_files():
     if sensScript != True:
         print("Creating emulator script file...")
         with open( __os.path.join(name,emulator), 'w' ) as ef:
-            ef.write("import gp_emu as g\n")
+            ef.write("import gp_emu_uqsa as g\n")
             ef.write("\n")
             ef.write("emul = g.setup(\""+config+"\")\n")
             ef.write("g.train(emul, auto=True)\n")
     else:
         print("Creating emulator + sensitivity script file...")
         with open( __os.path.join(name,emulator), 'w' ) as ef:
-            ef.write("import gp_emu as g\n")
-            ef.write("import gp_emu.sensitivity as s\n")
+            ef.write("import gp_emu_uqsa as g\n")
+            ef.write("import gp_emu_uqsa.sensitivity as s\n")
             ef.write("\n")
             ef.write("emul = g.setup(\""+config+"\")\n")
             ef.write("g.train(emul, auto=True)\n")
