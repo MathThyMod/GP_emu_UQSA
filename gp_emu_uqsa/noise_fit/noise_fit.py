7977338196354ede0f4156a58d17d24858f8eb2e
samcoveney <coveney.sam@gmail.com>
2017-03-21 15:12:34 +0000

    Tidied noisefit function

--- gp_emu_uqsa/noise_fit/noise_fit.py
+++ gp_emu_uqsa/noise_fit/noise_fit.py
@@ -22,7 +22,7 @@ def __untransform(x, reg="log"):
 
 
 # currently works only for 1D data
-def noisefit(data, noise, stopat=20):
+def noisefit(data, noise, stopat=20, olhcmult=100):
     
     ## NOTES
     #### currently no_retrain is False so we would always train to final set...
@@ -55,17 +55,6 @@ def noisefit(data, noise, stopat=20):
     #GD = g.setup(data, datashuffle=False, scaleinputs=False)
     x = GD.training.inputs # values of the inputs
     t = GD.training.outputs # values of the noisy outputs
-
-    #x_range = np.array( (np.linspace(np.amin(x), np.amax(x), t.size),) ).T
-    ## use an OLHC design for x_values of noise guesses we'll save
-    print("\nGenerating input points to predict noise values at...")
-    n = x[0].size * 100 
-    N = int(n/2)
-    olhc_range = [ [np.amin(col), np.amax(col)] for col in x.T ]
-    #print("olhc_range:", olhc_range)
-    filename = "x_range_input"
-    _gd.optLatinHyperCube(x[0].size, n, N, olhc_range, filename)
-    x_range = np.loadtxt(filename) # read generated oLHC file in
 
     #print(np.amin(x), np.amax(x))
     g.train(GD, no_retrain=False)
@@ -119,19 +108,6 @@ def noisefit(data, noise, stopat=20):
         p_GN = __emuc.Posterior(xp_GN, GN.training, GN.par, GN.beliefs, GN.K)
         r = __untransform(p_GN.mean)
 
-        ## save data to file
-        x_plot = __emuc.Data(x_range, None, GN.basis, GN.par, GN.beliefs, GN.K)
-        p_plot = __emuc.Posterior(x_plot, GN.training, GN.par, GN.beliefs, GN.K)
-        mean_plot = p_plot.mean
-        p_plot.interval()
-        UI, LI = p_plot.UI, p_plot.LI
-        print("\nSaving results to file...")
-        np.savetxt('noise-inputs', x_range )
-          #[ mean_plot, np.sqrt(__untransform(mean_plot)),\
-          #np.sqrt(__untransform(LI)), np.sqrt(__untransform(UI)) ] )
-        #np.savetxt('noise-outputs', mean_plot )
-        np.savetxt('noise-outputs', np.sqrt(__untransform(mean_plot)) )
-
         #GD = g.setup(data, datashuffle=False, scaleinputs=False)
         GD.training.set_r(r)
         g.train(GD, no_retrain=False)
@@ -139,6 +115,31 @@ def noisefit(data, noise, stopat=20):
         # break when we've done 'stopat' fits
         if count == stopat:
             print("\nCompleted", count, "fits, stopping here.")
+
+            #x_range = np.array( (np.linspace(np.amin(x), np.amax(x), t.size),) ).T
+            ## use an OLHC design for x_values of noise guesses we'll save
+            print("\nGenerating input points to predict noise values at...")
+            n = x[0].size * 100 
+            N = int(n/2)
+            olhc_range = [ [np.amin(col), np.amax(col)] for col in x.T ]
+            #print("olhc_range:", olhc_range)
+            filename = "x_range_input"
+            _gd.optLatinHyperCube(x[0].size, n, N, olhc_range, filename)
+            x_range = np.loadtxt(filename) # read generated oLHC file in
+
+            ## save data to file
+            x_plot = __emuc.Data(x_range, None, GN.basis, GN.par, GN.beliefs, GN.K)
+            p_plot = __emuc.Posterior(x_plot, GN.training, GN.par, GN.beliefs, GN.K)
+            mean_plot = p_plot.mean
+            p_plot.interval()
+            UI, LI = p_plot.UI, p_plot.LI
+            print("\nSaving results to file...")
+            np.savetxt('noise-inputs', x_range )
+              #[ mean_plot, np.sqrt(__untransform(mean_plot)),\
+              #np.sqrt(__untransform(LI)), np.sqrt(__untransform(UI)) ] )
+            #np.savetxt('noise-outputs', mean_plot )
+            np.savetxt('noise-outputs', np.sqrt(__untransform(mean_plot)) )
+
             break
 
     return None
